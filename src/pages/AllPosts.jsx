a25e8ed660655f677bec6404558df13130c35a52
ritesh0304 <ranaritesh0304@gmail.com>
2024-03-22 19:56:43 +0530

    initialisation

---
+++ src/pages/AllPosts.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import { Container, PostCard } from '../components';
+import appwriteService from '../appwrite/config';
+
+function AllPosts() {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            try {
+                const fetchedPosts = await appwriteService.getPosts([]);
+                if (fetchedPosts) { 
+                    // console.log(fetchedPosts)
+                    setPosts(fetchedPosts.documents);
+                }
+            } catch (error) {
+                console.error('Error fetching posts:', error);
+            }
+        };
+
+        fetchPosts();
+    }, []); // Empty dependency array ensures this effect runs only once on component mount
+
+    return (
+        <div className="w-full py-8">
+            <Container>
+                <div className="flex flex-wrap">
+                    {posts.map((post) => (
+                        <div key={post.$id} className="p-2 w-1/4">
+                            <PostCard {...post} />
+                        </div>
+                    ))}
+                </div>
+            </Container>
+        </div>
+    );
+}
+
+export default AllPosts;
